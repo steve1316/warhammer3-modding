7c81ef744e8505bc5d3411e72f3ad4a2941a7341
Steve Tu <stevetu11@yahoo.com>
2025-06-19 14:43:49 -0700

    Add spawn coordinates for the Immortal Empires Expanded campaign

---
+++ mods/land_encounters_and_points_of_interest_with_mct/script/land_encounters/constants/coordinates/immortal_empires_expanded/points_of_interest.lua
@@ -0,0 +1,9 @@
+local ieee_points_of_interest = {
+    ["placeholder"] = {
+        ["smithies"] = {},
+        ["taverns"] = {},
+        ["resources"] = {}
+     },
+}
+
+return ieee_points_of_interest
---
+++ mods/land_encounters_and_points_of_interest_with_mct/script/land_encounters/constants/coordinates/immortal_empires_expanded/treasures_and_spots.lua
@@ -0,0 +1,108 @@
+-- Manually acquired on 2025-02-03.
+local iee_coordinates = {
+    -- These IEEE-specific coordinates will not ever be used with the original system.
+    -- In addition, these are intended to be concatenated with the vanilla IE coordinates since they share the same map with the new map additions by IEEE.
+    ["placeholder"] = {
+        -- The Spice Route
+        {1110, 383},
+        -- Land of a Thousand Gods
+        {1114, 335},
+        {1160, 334},
+        -- Shambani Foothills
+        {1204, 309},
+        {1148, 307},
+        -- Central Ind
+        {1164, 259},
+        -- Ind's End
+        {1135, 251},
+        {1102, 248},
+        {1076, 217},
+        {1050, 222},
+        {1047, 154},
+        -- Eastern Colonies
+        {1015, 137},
+        {1031, 121},
+        -- Forest of Broken Fangs
+        {1312, 303},
+        {1230, 278},
+        -- Whispering Jungle
+        {1379, 295},
+        -- Cochincathay
+        {1371, 247},
+        {1364, 222},
+        {1402, 179},
+        -- Bloodied Coast
+        {1226, 236},
+        {1213, 205},
+        {1263, 209},
+        -- Kureshi Hills
+        {1311, 131},
+        {1290, 110},
+        {1250, 119},
+        {1264, 82},
+        {1307, 79},
+        {1310, 114},
+        -- Hokusei Arc
+        {1452, 388},
+        {1455, 349},
+        -- Hokurikudo
+        {1505, 342},
+        -- Samurai's Domain
+        {1445, 301},
+        -- Sun's Origin
+        {1455, 273},
+        {1441, 259},
+        {1506, 216},
+        -- Eastern Circuit
+        {1497, 278},
+        {1522, 269},
+        {1513, 252},
+        {1521, 202},
+        -- North Isle
+        {1462, 129},
+        -- Lost Isles of Elithis
+        {1481, 71},
+        {1452, 21},
+        -- Green Steppes
+        {1157, 793},
+        {1204, 769},
+        {1233, 753},
+        -- Deep Steppes
+        {1165, 817},
+        {1185, 836},
+        {1234, 779},
+        {1225, 817},
+        {1253, 823},
+        -- East Kurgania
+        {1286, 793},
+        {1259, 754},
+        {1294, 744},
+        {1315, 762},
+        {1341, 734},
+        -- Grave of Scythia
+        {1388, 743},
+        {1378, 778},
+        {1401, 742},
+        {1437, 764},
+        -- The Black Wastes
+        {1441, 723},
+        {1458, 746},
+        -- West Hunguria
+        {1469, 782},
+        {1493, 747},
+        {1530, 762},
+        {1539, 814},
+        -- North Ilhan
+        {1409, 694},
+        {1468, 656},
+        {1388, 609},
+        -- East Ilhan
+        {1468, 656},
+        {1438, 569},
+        -- South Ilhan
+        {1417, 588},
+        {1385, 566},
+    },
+}
+
+return iee_coordinates
